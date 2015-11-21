bf8bb9b08080b940239d57921a5103747f15a11b
Nathan Bolser <nbolser@gmail.com>
2015-11-20 21:46:07 -0700

    Add stats endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'stats', to: "stats#index"
+
   resources :orders, index: [:index ]
   resources :tables, except: [:new, :edit] do
     resources :orders, only: [:create] do
